e88f89ee48f5236444045a215cf6182b002ca1db
Daniel A. A. Pelsmaeker <developer@pelsmaeker.net>
2024-09-07 23:58:29 +0200

    Simplify template

--- docs/content/deploying.md
+++
@@ -1,7 +0,0 @@
----
-title: "Deploying"
----
-# Deploying
-This documentation can be deployed on Github (`.github/workflows/documentation.yml`)
-or Gitlab (`.gitlab-ci.yml`).
-
--- docs/tools/formatters.py
+++
@@ -1,8 +0,0 @@
-from markdown import util as md_util
-import xml.etree.ElementTree as etree
-
-def inline_span_format(source="", language="gui", class_name="guilabel", md=None):
-    """Inline generic span formatter."""
-    el = etree.Element('span', {'class': class_name})
-    el.text = md_util.AtomicString(source.replace(">", "‣"))
-    return el
