1a41633e338794914bd11e5bccb1c592074139aa
Daniel A. A. Pelsmaeker <developer@pelsmaeker.net>
2025-07-06 20:43:08 +0200

    Fix venv not activated correctly

--- docs/flake.nix
+++ docs/flake.nix
@@ -13,34 +13,6 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
-
-        runScript = pkgs.writeShellScriptBin "run-script" ''
-          #!/usr/bin/env bash
-          
-          # Create and activate the virtual environment
-          test -d .venv || ${pkgs.uv}/bin/uv venv .venv
-          source .venv/bin/activate
-
-          # Create a fancy welcome message
-          REPO_NAME=$(basename "$PWD")
-          PROPER_REPO_NAME=$(echo "$REPO_NAME" | awk '{print toupper(substr($0,1,1)) tolower(substr($0,2))}')
-          figlet "$PROPER_REPO_NAME"
-          echo "Welcome to the $PROPER_REPO_NAME development environment on ${system}!"
-          echo 
-
-          # Install Python packages from requirements.txt using uv
-          # This allows flexibility to use the latest PyPI packages
-          # Note: This makes the environment less deterministic
-          echo "- Installing pip packages with uv..."
-          if uv pip install --upgrade pip --quiet && \
-            uv pip install -r requirements.txt --quiet; then
-              package_count=$(pip list --format=freeze | wc -l)
-              echo "- Done. $package_count pip packages installed."
-          else
-              echo "Warning: An error occurred during uv pip setup."
-          fi
-        '';
-
       in {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
@@ -49,7 +21,30 @@
             figlet         # ASCII art welcome messages
           ];
           shellHook = ''
-            ${runScript}/bin/run-script
+            #!/usr/bin/env bash
+            
+            # Create and activate the virtual environment
+            test -d .venv || ${pkgs.uv}/bin/uv venv .venv
+            source .venv/bin/activate
+
+            # Create a fancy welcome message
+            REPO_NAME=$(basename "$PWD")
+            PROPER_REPO_NAME=$(echo "$REPO_NAME" | awk '{print toupper(substr($0,1,1)) tolower(substr($0,2))}')
+            figlet "$PROPER_REPO_NAME"
+            echo "Welcome to the $PROPER_REPO_NAME development environment on ${system}!"
+            echo 
+
+            # Install Python packages from requirements.txt using uv
+            # This allows flexibility to use the latest PyPI packages
+            # Note: This makes the environment less deterministic
+            echo "- Installing pip packages with uv..."
+            if uv pip install --upgrade pip --quiet && \
+              uv pip install -r requirements.txt --quiet; then
+                package_count=$(pip list --format=freeze | wc -l)
+                echo "- Done. $package_count pip packages installed."
+            else
+                echo "Warning: An error occurred during uv pip setup."
+            fi
           '';
         };
       }
